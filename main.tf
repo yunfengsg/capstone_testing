5244100e20f0f349bef18416411b1a1739ef810c
Xinwei_GH <xinwei.cheng.88@gmail.com>
2025-03-29 12:42:39 +0800

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,29 @@
-module "lab01" {
-  source = "./labs/lab01"
+module "lab01-prod" {
+  source = "./prod/labs/lab01"
   # ...
 }
 
-module "lab02" {
-  source = "./labs/lab02"
+module "lab02-prod" {
+  source = "./prod/labs/lab02"
   # ...
 }
 
-module "lab03" {
-  source = "./labs/lab03"
+module "lab03-prod" {
+  source = "./prod/labs/lab03"
+  # ...
+}
+
+module "lab01-nonprod" {
+  source = "./nonprod/labs/lab01"
+  # ...
+}
+
+module "lab02-nonprod" {
+  source = "./nonprod/labs/lab02"
+  # ...
+}
+
+module "lab03-nonprod" {
+  source = "./nonprod/labs/lab03"
   # ...
 }
